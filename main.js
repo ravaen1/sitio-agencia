541a1dcbbb0a3b61077c28003c96e191a49be071
rai <ravaen1001@gmail.com>
2021-11-25 08:16:23 -0500

    js script

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = document.getElementById('typewriter');
+const typewriter = new Typewriter(app, {
+    loop:true,
+    delay: 75
+});
+
+typewriter
+.typeString('La capital del sol')
+.pauseFor(200)
+.start();
